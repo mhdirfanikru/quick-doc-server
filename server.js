9039ee6a98ff963e30f4140dfe941e56b91dc965
mhdirfanpn <mhdirfanpn.com>
2023-05-12 12:02:39 +0530

    second

---
+++ server.js
@@ -0,0 +1,112 @@
+import express from "express";
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import morgan from 'morgan'
+import dotenv from 'dotenv'
+import dbConnection from './config/db.js'
+import userRoutes from './routes/userRoutes.js'
+import adminRoutes from './routes/adminRoutes.js'
+import doctorRoutes from './routes/doctorRoutes.js'
+import paymentRoutes from './routes/paymentRoutes.js'
+import conversationRoutes from './routes/conversationRoutes.js'
+import messageRoutes from './routes/messageRoutes.js'
+import http from 'http';
+import { Server } from 'socket.io'
+
+const app=express();
+dotenv.config();
+
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000"
+    },
+});
+
+app.use(cors({
+  origin: '*',
+  optionsSuccessStatus: 200
+}));
+
+app.use(morgan("dev"))
+app.use(bodyParser.json());
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+
+app.use('/api',userRoutes)
+app.use('/api/doc',doctorRoutes)
+app.use('/api/admin',adminRoutes)
+app.use('/api/payment',paymentRoutes)
+app.use('/api/conversation',conversationRoutes)
+app.use('/api/message',messageRoutes)
+
+app.get('*',(req,res)=>{
+    res.status(404).send("PAGE NOT FOUND")
+})
+
+try {
+    
+let users = [];
+console.log(users);
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId == userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  console.log(users);
+  return users.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  //when connect
+  console.log("socket connected");
+
+  //take userId and socketId from user
+  socket.on("addUser", (userId) => {
+    try {
+      addUser(userId, socket.id);
+      io.emit("getUsers", users);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  //send and get messages
+  socket.on("sendMessage", ({ senderId, recieverId, text }) => {
+    try {
+      console.log(senderId, recieverId, text);
+      const user = getUser(recieverId);
+      io.to(user.socketId).emit("getMessage", {
+        senderId,
+        text,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  //when disconnect
+  socket.on("disconnect", () => {
+    try {
+      removeUser(socket.id);
+      io.emit("getUsers", users);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+});
+
+} catch (error) {
+    console.log(`error - ${error.message}`)
+}
+
+dbConnection().then(()=>{
+    server.listen(process.env.PORT,()=>console.log(`SERVER STARTED AT PORT:700`))
+})
