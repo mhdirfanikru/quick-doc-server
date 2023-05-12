4ea8ff07263ec712e18d1097de4a0b7a0f950e7a
mhdirfanpn <mhdirfanpn.com>
2023-05-12 17:42:02 +0530

    [update] => websocket frontend origin added

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ dotenv.config();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000"
+        origin: "https://www.quickdoc.online"
     },
 });
 
